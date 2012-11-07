6f1f0f0e23966434499ce1189302e76f55c541bd
Brandon Snuggs <bsnuggs1@gmail.com>
2012-11-07 16:26:19 -0500

    Still have to make conversions for the accel and gyro.

---
+++ contiki-2.6/apps/test/mpu-6050.h
@@ -0,0 +1,12 @@
+#ifndef _MPU6050_H_
+#define _MPU6050_H_
+
+#define MPU6050_ACCEL               0x3b
+#define MPU6050_TEMP                0x41
+#define MPU6050_GYRO                0x43
+#define MPU6050_SLEEP               0x6b
+#define MPU6050_ADDRESS             0x68
+#define MPU6050_RA_WHO_AM_I         0x75
+
+
+#endif /* __MPU6050_H_ */
